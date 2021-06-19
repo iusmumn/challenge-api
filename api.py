5e1c6206579e18ee4e34d6cc565904d2ed5d25ec
Raphael Santo Domingo <raph@moov.io>
2021-06-19 00:00:12 -0500

    Update endpoints

--- api.py
+++ api.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     """A base endpoint just because"""
-    return 'Hello, from Challenge API!'
+    return 'Hello, this is the root (or base) endpoint!'
 
 @app.route('/health')
 def health():
     """A health endpoint to check if the api is running"""
-    return 'Ok!!!'
+    return 'OK'
 
 if __name__ == '__main__':
     app.run()
