4543a65906e588c30e7a8b26de688cac6eefe741
Jinghao Ren <ren@ebm-er-10-131-70-180.wireless.umn.edu>
2021-06-25 16:59:53 -0500

    save to my work for challenge

--- api.py
+++ api.py
@@ -1,14 +1,34 @@
+import db
+import json
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     """A base endpoint just because"""
-    return 'Hello, from Challenge API!'
-    
-def ren():
-    "ren test"
-    return 'Hi, Ren is here'
+    return 'Hello, this is the root (or base) endpoint!'
+
+@app.route('/health')
+def health():
+    """A health endpoint to check if the api is running"""
+    return 'OK'
+
+@app.route('/users')
+def users():
+    """A users endpoint to grab user data from the database"""
+    print("Grabbing user data from database")
+    conn = db.connect()
+    users = db.get_users(conn)
+    return json.dumps(users)
+
+@app.route('/challenges')
+def challenges():
+    """A challenges endpoint to grab challenge data from the datapages"""
+    print("Grabbing challenge data from database")
+    conn = db.connect()
+    challenges = db.get_challenges(conn)
+    return json.dumps(challenges)
 
 if __name__ == '__main__':
     app.run()
